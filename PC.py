0ac0561134e14a385448c099a1ef32a6c6917f1f
Heraldo@192.168.0.195 <heraldo@heraldo-OptiPlex-990>
2023-12-07 09:43:48 -0500

    This is a work on strings

---
+++ PC.py
@@ -0,0 +1,26 @@
+# Script Name:                  Python Collections
+# Author:                       Heraldo Morales
+# Date of latest revision:      12/07/23
+# Purpose:                      A fundamental grasp of data structures and algorithms
+
+
+
+
+
+# Assigning a list of ten string elements
+my_list = ["cars", "ovo", "money", "studies", "books", "test", "dirt", "tech", "honesty", "raves"]
+
+# Printing the fourth element of the list
+print("Fourth element:", my_list[3])
+
+# Printing the sixth through tenth element of the list
+print("Sixth to tenth elements:", my_list[5:10])
+
+# Changing the value of the seventh element to "onion"
+if len(my_list) >= 7:
+    my_list[6] = "onion"
+
+# Displaying the modified list
+print("Modified list:", my_list)
+
+
